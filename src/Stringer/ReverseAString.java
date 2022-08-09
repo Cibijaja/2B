a3735d518f6f4d1c087116797933925e65f0f7b4
Cibijaja <cibijaja@gmail.com>
2022-08-09 15:04:09 +0530

    second

---
+++ src/Stringer/ReverseAString.java
@@ -0,0 +1,19 @@
+package Stringer;
+
+public class ReverseAString {
+	
+	public static void main(String[] args) {
+		String s ="hello";
+		String rev ="";
+		
+		
+		for (int i = s.length()-1; i >= 0; i--) {
+			char c = s.charAt(i);
+			rev =rev + c;
+		}
+		System.out.println(rev);
+		
+	}
+	
+
+}
