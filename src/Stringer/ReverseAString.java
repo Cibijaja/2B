c9dbc5c06805d9419eb8e38ca5aa53ba6d9a6fe4
Cibijaja <cibijaja@gmail.com>
2022-08-09 16:14:30 +0530

    fourth

--- src/Stringer/ReverseAString.java
+++ src/Stringer/ReverseAString.java
@@ -5,8 +5,7 @@ public class ReverseAString {
 	public static void main(String[] args) {
 		String s ="hello";
 		String rev ="";
-		
-		String t ="Hi";
+		String c;
 		
 		
 		for (int i = s.length()-1; i >= 0; i--) {
